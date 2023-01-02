e1a7d15e2ed128b26e764b8839ae5c87877f9614
Alif Wibowo <alif.likes.coding@gmail.com>
2023-01-03 04:09:54 +0700

    Add function logic

--- sub_strings.rb
+++ sub_strings.rb
@@ -1,5 +1,13 @@
+def sub_strings(string, dictionary)
+    string = string.downcase
+    hash = string.split(" ").reduce(Hash.new(0)) do |object, word|
+        dictionary.each { |element| object[element] += 1 if  word.include?(element)}
+        object
+    end
+    hash
+end
+
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 
-def sub_strings(strings, dictionary)
-    
-end
+p sub_strings("below", dictionary)
+p sub_strings("Howdy partner, sit down! How's it going?", dictionary)
